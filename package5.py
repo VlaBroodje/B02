a9c55d2453bc340bf934eb0b09ff7d8b0ffc1cb5
John Doe <danielnaydenov05@gmail.com>
2025-12-16 14:30:54 +0100

    test67

--- package5.py
+++ package5.py
@@ -5,17 +5,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math
 
-
 # custom functions and constants
 from constants import *
 from functions import *
 from package4 import *
 
-ks=3
-bPlate = 4     #<------- idk
+#Уеб Бъклинг
+hf = zmax_front-zmin_front
+hr = zmax_rear-zmin_rear
+shearAvg = np.empty_like(y_linspace)
+for i,y in enumerate(y_linspace):
+    shearAvg[i] = V_func(y)/(hf*t+hr*t)
 
-shearCrit = (math.pi**2*k*Emod)/(12*(1-poission**2))*((t/bPlate)**2)
-hf = 1
-hr = 2
-shearAvg = V/(hf*t+hr*t)
+kv = 1.5 #<-------- chat
 
+def webBuckling():
+    return shearAvg*kv
+
+#Скин Бъклинг
+kc = 21
+bplate = 37
+def skinBuckling():
+    return (((math.pi**2)*kc*Emod)/(12*(1-poission**2)))*((t/bplate)**2)
+
+K = 32
+#Колъмн Бъклинг
+def kolumnBuckling():
+    return K*(math.pi**2)*Emod*I/()
+#Глобал Бъклинг
