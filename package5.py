97bcced18f5656857548f6fc9ab4a84f60d9e49f
John Doe <danielnaydenov05@gmail.com>
2025-12-16 15:01:21 +0100

    removed bs2-test-plz work

--- package5.py
+++ package5.py
@@ -11,11 +11,4 @@ from constants import *
 from functions import *
 from package4 import *
 
-ks=3
-bPlate = 4     #<------- idk
-
-shearCrit = (math.pi**2*k*Emod)/(12*(1-poission**2))*((t/bPlate)**2)
-hf = 1
-hr = 2
-shearAvg = V/(hf*t+hr*t)
 
